287d289f4293b3a4db4961982a37617ce2f6828a
Love <parlove@student.chalmers.se>
2017-01-24 14:32:23 +0100

    Added script used to include the libraries into Simulink.

---
+++ Libraries/slblocks.m
@@ -0,0 +1,23 @@
+
+function blkStruct = slblocks
+		% This function specifies that the library should appear
+		% in the Library Browser
+		% and be cached in the browser repository
+
+		Browser.Library = 'cube_lib';
+		% 'mylib' is the name of the library
+
+		Browser.Name = 'Cubex 2D';
+		% 'My Library' is the library name that appears in the Library Browser
+
+		blkStruct.Browser = Browser; 
+
+
+
+% %At the MATLAB® command prompt, enter this command to enable the model property EnableLBRepository. 
+% Your library can appear in the browser only if this property is on when you save your library.
+%
+% Comment: Seems that the library must be open when doing this 
+% %set_param(gcs,'EnableLBRepository','on');
+% 
+% 
