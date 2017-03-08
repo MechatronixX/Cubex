24c275db3a81b9b9d9a38b9915b4efa53a2c2dec
Mr Nitram <davidwa@student.chalmers.se>
2017-03-08 15:21:36 +0100

    Init rotation matrix to be able to run in simulink

--- Libraries/Magdwick/quaternion_library/quatern2euler.m
+++ Libraries/Magdwick/quaternion_library/quatern2euler.m
@@ -11,7 +11,7 @@ function euler = quatern2euler(q)
 %
 %	Date          Author          Notes
 %	27/09/2011    SOH Madgwick    Initial release
-
+    R = zeros(3,3);
     R(1,1,:) = 2.*q(:,1).^2-1+2.*q(:,2).^2;
     R(2,1,:) = 2.*(q(:,2).*q(:,3)-q(:,1).*q(:,4));
     R(3,1,:) = 2.*(q(:,2).*q(:,4)+q(:,1).*q(:,3));
@@ -22,6 +22,6 @@ function euler = quatern2euler(q)
     theta = -atan(R(3,1,:) ./ sqrt(1-R(3,1,:).^2) );
     psi = atan2(R(2,1,:), R(1,1,:) );
 
-    euler = [phi(1,:)' theta(1,:)' psi(1,:)'];
+    euler = [phi(1,:)' theta(1,:)' psi(1,:)']';
 end
 
