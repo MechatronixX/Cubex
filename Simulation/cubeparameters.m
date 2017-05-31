4bf090d43d42ab0eead63895333d4906f543c132
Love <parlove@student.chalmers.se>
2017-05-31 11:25:33 +0200

    Removed wheel.l as to only have one single definition of how long the side of the cube is.

--- Simulation/cubeparameters.m
+++ Simulation/cubeparameters.m
@@ -70,31 +70,15 @@ wheel = struct( 'm',        273*g_,...     % Wheel mass  % Björn-Erik: Acc to CA
       
 % I_wheel: Multiply by factor<1 to compensate for that the wheel does not
 % have all its mass in the outer circle.
-% Close to I_wheel = 8*10^-4 Björn-Erik: Acc to CAD model (from Linearized_system.m)
 wheel.J     = wheel.m*(wheel.radius*0.9)^2;
 wheel.Iw0    = (1/12)*wheel.m*(3*wheel.radius^2 + wheel.h^2); 
-
-%The wheel spins around its z-axis in its own coordinate system 
-wheel.Iz = wheel.J;  
-wheel.Theta_w0 = diag(wheel.Iw0*ones(3,1));
-wheel.Theta_z  = diag(wheel.Iz*ones(3,1));
-
-%TODO: These vectors have the right structure but  should be a function of
-%cube.r 
-wheel.rw1               = [0 ; wheel.l ; wheel.l];
-wheel.rw2               = [wheel.l ; 0 ; wheel.l];
-wheel.rw3               = [wheel.l ; wheel.l ; 0];
-wheel.rw                = wheel.rw1 + wheel.rw2 + wheel.rw3;
-
-wheel.I_tilde_2         = -wheel.m.*skew_matrix(wheel.rw1)^2;
-wheel.I_tilde_3         = -wheel.m.*skew_matrix(wheel.rw2)^2;
-wheel.I_tilde_4         = -wheel.m.*skew_matrix(wheel.rw3)^2;
 
 
 %---------Wheel damping
 % TODO: Employ system identification to get this better
 %Set quadratic damping coeffcieint such that 1 Nm of torque input gives 
-%equilibrium at 10 000 RPM ~ 1000 Rad/s 
+%equilibrium at 10 000 RPM ~ 1000 Rad/s. The quadratic term was ditched in
+%favor or a simpler linear damping model, look into this 
 
 %wheel.b = 1/1000^2; 
 wheel.bq = 60/1000^2;
@@ -117,29 +101,49 @@ cube = struct( 'm_tot',        [] ,...
                'tensor',       [],...
                'I3D',          [],...   %3D equivalent inertia tensor, when all mass displacement, wheels etc are included
                'I3D_tilde',    [],...     
-               'rcb',          [],...   %Vector from corner to center of gravity  
+               'rcb',          [],...   %Vector from corner to center of gravity of the cube casing  
                'I_tilde_1',    [] );
            
 %Seems that struct members must be initalized like this when they depend on
 %one another
-% cube.m_tot             = 3.487*kg_;                    %Complete cube mass, wheels batteries and all  
-cube.m_tot             = 2900*g_;                    %Complete cube mass, wheels batteries and all 
-cube.l                 = 180*mm_;                      %Length of one side
+% cube.m_tot             = 3.487*kg_;                   %Complete cube mass, wheels batteries and all  
+cube.m_tot             = 2900*g_;                       %Complete cube mass, wheels batteries and all 
+cube.l                 = 180*mm_;                       %Length of one side
 cube.r                 = cube.l/2; 
-cube.rcb               = [cube.r; cube.r ; cube.r]; %Vector from cube corner -> COG
-cube.Ix                = 1/6*cube.m_tot*cube.l^2;      %Principal inertia for cuboid w. evenly distrib mass
+cube.rcb               = [cube.r; cube.r ; cube.r];     %Vector from cube corner -> COG
+cube.Ix                = 1/6*cube.m_tot*cube.l^2;       %Principal inertia for cuboid w. evenly distrib mass
 cube.Iy                = cube.Ix; 
 cube.Iz                = cube.Ix;
-cube.l_corner2cog      = sqrt(2)*cube.l*0.5;               %From a corner to the centerpoint
+cube.l_corner2cog      = sqrt(2)*cube.r;                              %From a corner to the centerpoint in the plane of a side
 cube.I_2D              = cube.Iy+cube.m_tot*cube.l_corner2cog^2;      %Convenience inertia when edge balancing on an edge
 cube.Ic                = cube.Ix; 
 cube.tensor            = diag([cube.Ic,cube.Ic,cube.Ic]); 
 cube.I_tilde_1         = -cube.m_tot.*skew_matrix(cube.rcb)^2;
+
+
+                     
+                     
+% The position vectors from the corner to the center of gravity of the
+% wheels is a function of the side
+wheel.rw1               = [0 ; cube.r ; cube.r];
+wheel.rw2               = [cube.r ; 0 ; cube.r];
+wheel.rw3               = [cube.r ; cube.r ; 0];
+wheel.rw                = wheel.rw1 + wheel.rw2 + wheel.rw3;
+
+wheel.I_tilde_2         = -wheel.m.*skew_matrix(wheel.rw1)^2;
+wheel.I_tilde_3         = -wheel.m.*skew_matrix(wheel.rw2)^2;
+wheel.I_tilde_4         = -wheel.m.*skew_matrix(wheel.rw3)^2;  
+
+%The wheel spins around its z-axis in its own coordinate system 
+wheel.Iz = wheel.J;  
+wheel.Theta_w0 = diag(wheel.Iw0*ones(3,1));
+wheel.Theta_z  = diag(wheel.Iz*ones(3,1));
+
 cube.I3D               = cube.I_tilde_1 + wheel.I_tilde_2 + wheel.I_tilde_3 +...
                          wheel.I_tilde_4 + cube.tensor + wheel.Theta_w0;
 cube.I3D_tilde         = cube.I_tilde_1 + wheel.I_tilde_2 + wheel.I_tilde_3 +...
                          wheel.I_tilde_4;
-
+                     
 %% Motors 
 %EC45 motor datasheet http://www.maxonmotor.com/maxon/view/news/MEDIENMITTEILUNG-EC-45-flat-70-Watt
 
@@ -185,14 +189,3 @@ imu = struct('a_max', single(2*9.81),...                    % Saturation for the
                           -0.7731  -0.6333 0.0359
                           0.0404  0.0074  0.9992])); % Rotation matrix IMU3      
              
-           
-% %I cannot get the script to run while this stuff is in here
-% function S = skew_matrix(P)
-% %   argument P most be a position vector in three dimenstion
-% %   return a skew_matrix
-% assert(size(P,1) == 3,'Argurment P most be of size 3x1')
-% 
-% S = [ 0     -P(3)    P(2);
-%      P(3)     0     -P(1);
-%     -P(2)    P(1)      0];
-% end
